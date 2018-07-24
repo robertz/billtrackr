9b3be72bf74d1ec50e3734d8f9d16ee3274c8dd5
Robert Zehnder <robert@kisdigital.com>
2018-07-24 14:24:37 -0400

    Updating index page with screenshots

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,51 @@
 <template>
-  <v-jumbotron
-    src="/images/aidan-bartos-313782-unsplash.jpg"
-    dark>
-    <v-container
-      fill-height>
-      <v-layout align-center>
-        <v-flex>
-          <h3 class="display-3">BillTrackr</h3>
-          <div class="subheading">Easily track who you pay each month</div>
-          <v-btn large color="primary" class="mx-0 my-3" @click="login()">Sign In</v-btn>
-        </v-flex>
-      </v-layout>
-    </v-container>
-  </v-jumbotron>
+  <v-container fluid>
+    <v-flex xs12>
+      <v-jumbotron
+        src="/images/christine-roy-343235-unsplash.jpg"
+        dark>
+        <v-container
+          fill-height>
+          <v-layout align-center>
+            <v-flex>
+              <h3 class="display-3">BillTrackr</h3>
+              <div class="subheading">Easily track who you pay each month</div>
+              <v-btn large color="primary" class="mx-0 my-3" @click="login()">Sign In</v-btn>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-jumbotron>
+    </v-flex>
+
+    <v-card>
+      <v-container
+        fluid
+        grid-list-md
+      >
+        <v-layout row wrap>
+          <v-flex
+            v-for="card in cards"
+            v-bind="{ [`xs${card.flex}`]: true }"
+            :key="card.title"
+          >
+            <v-card>
+              <v-card-media
+                :src="card.src"
+                height="300px"
+              ></v-card-media>
+              <v-card-title>
+                <div>
+                  <h3 headline mb-1>{{ card.title }}</h3>
+                  <div>{{ card.text }}</div>
+                </div>
+              </v-card-title>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-card>
+
+  </v-container>
 </template>
 
 <script>
@@ -24,7 +57,16 @@ export default {
     if (this.isAuthenticated) this.$router.push({ path: '/dashboard' })
   },
   data () {
-    return {}
+    return {
+      cards: [
+        { title: 'Dashboard', text: 'View your week at a glance along with some basic stats', src: '/images/screens/Dashboard.png', flex: 6 },
+        { title: 'Weekly Forecast', text: 'See the details you need to stay on top of your bills for the week', src: '/images/screens/Week.png', flex: 6 },
+        { title: 'Monthly Forecast', text: 'Prefer seeing your whole month at a glance? Track all your bills', src: '/images/screens/Month.png', flex: 6 },
+        { title: 'Payees', text: 'See everyone you pay in one easy screen along with APR and yearly totals', src: '/images/screens/Payee.png', flex: 6 },
+        { title: 'Detail', text: 'Need details for a specific bill? See your monthly average and yearly amounts. You can also see your payment history with totals', src: '/images/screens/Detail.png', flex: 6 },
+        { title: 'Payments', text: 'Quickly see your payment history', src: '/images/screens/Payments.png', flex: 6 }
+      ]
+    }
   },
   methods: {
     login () {

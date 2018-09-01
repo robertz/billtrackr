d969ff55869cf274ba5c35b1dacb2ebe2aa223a9
Robert Zehnder <robert@kisdigital.com>
2018-09-01 09:56:19 -0400

    Fixing deprecated components

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-nuxt generate
-cp now.json ./dist
-cd dist
 now && now alias && now rm billtrackr --safe --yes
-cd ..
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,20 @@
 <template>
   <v-container fluid>
     <v-flex xs12>
-      <v-jumbotron
+      <v-img
         src="/images/christine-roy-343235-unsplash.jpg"
-        dark>
+        max-height="400">
         <v-container
           fill-height>
           <v-layout align-center>
             <v-flex>
-              <h3 class="display-3">BillTrackr</h3>
-              <div class="subheading">Easily track who you pay each month</div>
+              <h3 class="display-3 white--text">BillTrackr</h3>
+              <div class="subheading white--text">Easily track who you pay each month</div>
               <v-btn large color="primary" class="mx-0 my-3" @click="login()">Sign In</v-btn>
             </v-flex>
           </v-layout>
         </v-container>
-      </v-jumbotron>
+      </v-img>
     </v-flex>
 
     <v-card>
@@ -57,14 +57,14 @@ export default {
   data () {
     return {
       cards: [
-        { title: 'Dashboard', text: 'See basic "Top 5" stats for your bills and quickly track what has been paid for the week', src: '/images/screens/Dashboard.png', flex: 4 },
-        { title: 'Weekly Forecast', text: 'See what is due, what is paid and what is left this week', src: '/images/screens/Week.png', flex: 4 },
-        { title: 'Monthly Forecast', text: 'Prefer seeing your whole month at a glance? See what is due, what is paid and what is left this month', src: '/images/screens/Month.png', flex: 4 },
-        { title: 'Bills List', text: 'See all your bills in one easy screen along with APR and yearly totals. Also see which bills are paid automatically so you never forget a bill', src: '/images/screens/Payee.png', flex: 4 },
-        { title: 'Create Bill', text: 'Enter in basic information to create a new bill and begin tracking it immediately', src: '/images/screens/NewBill.png', flex: 4 },
-        { title: 'Detailed Bill/Payment Information', text: 'See your monthly amount, monthly average and yearly amount as well as your payment history', src: '/images/screens/Detail.png', flex: 4 },
-        { title: 'Edit Bill', text: 'Something change? It is easy to edit the details of your bill', src: '/images/screens/EditBill.png', flex: 4 },
-        { title: 'Payment History', text: 'See all payments you have made', src: '/images/screens/Payments.png', flex: 4 }
+        { title: 'Dashboard', text: 'See basic "Top 5" stats for your bills and quickly track what has been paid for the week', src: '/images/screens/Dashboard.png', flex: 3 },
+        { title: 'Weekly Forecast', text: 'See what is due, what is paid and what is left this week', src: '/images/screens/Week.png', flex: 3 },
+        { title: 'Monthly Forecast', text: 'Prefer seeing your whole month at a glance? See what is due, what is paid and what is left this month', src: '/images/screens/Month.png', flex: 3 },
+        { title: 'Bills List', text: 'See all your bills in one easy screen along with APR and yearly totals. Also see which bills are paid automatically so you never forget a bill', src: '/images/screens/Payee.png', flex: 3 },
+        { title: 'Create Bill', text: 'Enter in basic information to create a new bill and begin tracking it immediately', src: '/images/screens/NewBill.png', flex: 3 },
+        { title: 'Detailed Bill/Payment Information', text: 'See your monthly amount, monthly average and yearly amount as well as your payment history', src: '/images/screens/Detail.png', flex: 3 },
+        { title: 'Edit Bill', text: 'Something change? It is easy to edit the details of your bill', src: '/images/screens/EditBill.png', flex: 3 },
+        { title: 'Payment History', text: 'See all payments you have made', src: '/images/screens/Payments.png', flex: 3 }
       ]
     }
   },

38e893d882e446098ef6ec0c2b0e28cd1087d5b2
Robert Zehnder <robert@kisdigital.com>
2018-09-05 12:53:48 -0400

    Only redirect on homepage if session has not expired

--- middleware/anonymous.js
+++ middleware/anonymous.js
@@ -1,6 +1,15 @@
 /* eslint-disable */
+import { unsetToken } from '~/utils/auth'
+import { logout } from '~/utils/lock'
+
 export default function ({ store, redirect }) {
   if (store.getters.isAuthenticated) {
+    // session was authenticated but has timed out
+    if (store.getters.loggedUser.exp < Date.now() / 1000) {
+      unsetToken()
+      logout()
+      return
+    }
     return redirect('/dashboard')
   }
 }
